b89216db7787a62b86621d2234d214a7ae826caa
nuno <75589883+nuno-src@users.noreply.github.com>
2023-09-13 16:59:44 +0100

    img to base64 and reverse

--- base64Converter.py
+++ base64Converter.py
@@ -16,12 +16,50 @@ def base64_to_text():
 
     b64_text = input("\nInsira a mensagem(base64): ")
     b_text = base64.b64decode(b64_text)
+    #print(b_text)
     text = b_text.decode()
 
     print(text)
 
 
 
+def img_to_base64t():
+    
+    with open("image.png", "rb") as f:
+        data = f.read()
+        
+    #print(data)
+    
+    #b_text = data.encode()
+    b64_text = base64.b64encode(data)
+    print(b64_text.decode('UTF-8'))
+
+
+
+
+def base64t_to_img():
+
+    b64_text = input("\nInsira a mensagem(base64): ")
+    
+    try:
+        data = base64.b64decode(b64_text)
+    except:
+        print("Erro!!! - mensagem muito grande")
+        
+    else:
+        pass
+    #print(data)
+
+    with open("file.png", "wb") as f:
+        f.write(data)
+
+    print("\n Complete!")
+    
+    
+    
+    
+    
+    
 if __name__ == '__main__':
 
     opc = ""
@@ -34,6 +72,8 @@ if __name__ == '__main__':
         print("===================================================")
         print("  [1] - Text to Base64")
         print("  [2] - Base64 to text")
+        print("  [3] - imagem to text Base64")
+        print("  [4] - Base64 text to imagem")
         print("  [exit]")
         print("===================================================")
 
@@ -44,6 +84,10 @@ if __name__ == '__main__':
             text_to_base64()
         elif opc == "2":
             base64_to_text()
+        elif opc == "3":
+            img_to_base64t()
+        elif opc == "4":
+            base64t_to_img()
         elif opc == "exit":
             print("\nA sair...")
         else:
