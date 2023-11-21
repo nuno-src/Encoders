ce1a699694f93e2f749b4565a915ea7d4ec5b2a8
nuno <75589883+nuno-src@users.noreply.github.com>
2023-11-21 18:43:02 +0000

    hex converter update
    now can convert decimal to hex and hex to decimal

--- Hexadecimal.py
+++ Hexadecimal.py
@@ -6,7 +6,7 @@ def text_to_hexa():
 
     print("\nHexadecimal code:")
     for c in list(text):
-        print(str(hex(ord(c)))[2:], end=" ")
+        print(str(hex(ord(c)))[2:].upper(), end=" ")
 
 
 
@@ -16,6 +16,31 @@ def hexa_to_text():
     print("\nText:")
     for c in list(hexatext.split()):
         print(chr(int(c, 16)), end="")
+
+
+def decimal_to_hex():
+    decimalt = 1
+    while decimalt != 0:
+    
+        try:
+            decimalt = int(input("\nType decimal numbers: "))
+
+            print("\nHexadecimal code:", hex(decimalt)[2:].upper())
+            print("\nPress 0 to exit")
+        except:
+            print("Insira apenas numeros!")
+  
+
+
+def hex_to_decimal():
+    hexat = ""
+    while hexat != "0":
+        
+        hexat = input("\nType Hexadecimal code: ")
+        dec_output = str(int(hexat, 16))
+        print("\nDecimal :", dec_output)
+        print("Press 0 to exit")
+    
 
 
 
@@ -32,6 +57,8 @@ if __name__ == '__main__':
         print("===================================================")
         print("  [1] - Text to Hexadecimal")
         print("  [2] - Hexadecimal to text")
+        print("  [3] - Decimal to Hex")
+        print("  [4] - Hex to Decimal")
         print("  [exit]")
         print("===================================================")
 
@@ -42,7 +69,16 @@ if __name__ == '__main__':
             text_to_hexa()
         elif opc == "2":
             hexa_to_text()
+        elif opc == "3":   
+            decimal_to_hex()
+        elif opc == "4":   
+            hex_to_decimal()
         elif opc == "exit":
             print("\nA sair...")
         else:
             print("\nERROR! - INVALID OPTION!")
+            
+            
+            
+            
+            
